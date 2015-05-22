a4b3b2c4973f065752bb390c5a484e498f9b2f1f
MikeWasHere <xtrjago@gmail.com>
2015-05-21 20:40:56 -0400

    WMB website

---
+++ WMB website/CodeIgniter-3.0.0/application/views/view_home.php
@@ -0,0 +1,33 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Home Page</title>
+
+</head>
+<body>
+
+<div id="container">
+	<div id="nav">
+		<ul>
+			<li><a href="#"></a></li>
+			<li><a href="#"></a></li>
+			<li><a href="#"></a></li>
+		</ul>
+	</div>	
+	
+	<div id="content">
+		<h1>Home Page</h1>
+		<p>Welcome to my awesome site</p>
+	</div>
+	
+	<div id="footer">
+		<p>Copyright (c) 2012 basicsite.com. All rights reserved.</p>
+	</div>
+	
+</div>
+
+</body>
+</html>
---
+++ WMB website/basicSite/application/views/view_home.php
@@ -0,0 +1,25 @@
+
+<body>
+
+<div id="container">
+	<div id="nav">
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+	</div>	
+	
+	<div id="content">
+		<h1>Home Page</h1>
+		<p>Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'? Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain's going to be? He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass.</p>
+	</div>
+	
+	<div id="footer">
+		<p>Copyright (c) 2012 basicsite.com. All rights reserved.</p>
+	</div>
+	
+</div>
+
+</body>
+</html>
